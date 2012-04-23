1e4188f47b371d498bab84f2c6e01906514b1b31
motemen <motemen@gmail.com>
2012-04-23 22:01:45 +0900

    eg

---
+++ eg/skeleton/pm/files/lib/__PATH__.pm
@@ -0,0 +1,33 @@
+package __NAMESPACE__;
+use strict;
+use warnings;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+__NAMESPACE__ - 
+
+=head1 SYNOPSIS
+
+  use __NAMESPACE__;
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
